ade7c05c86743019593372e0f6600dfafa891422
lborou <zhuxingnuo@yahoo.co.jp>
2021-05-05 21:43:45 +0900

    v3

--- HelloWord.java
+++ HelloWord.java
@@ -2,5 +2,6 @@ class HelloWorld {
  public static void main(String[] args) {
  System.out.println("Hello, world.");
  System.out.println("Hello, world.20210505");
+ System.out.println("Hello, world.20210505 v3");
  }
 }
